eef0bb0e40b4f90c7041d28304e1eddd4367b94c
Maddie <mrheadric@gmail.com>
2018-02-20 23:33:54 -0500

    my new interaction
    its a noun

---
+++ files/page_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s],t,_(u,v,w,x,y,z,A,_(),B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,F,N,F,O,P,Q,null,R,S,T,U,V,W,X,S),Y,_(),Z,_(),ba,_(bb,[])),bc,_(),bd,_());}; 
+var b="url",c="page_1.html",d="generationDate",e=new Date(1518492696712.77),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="onClickM",o="onClickI",p="onClickN",q="onClickU",r="onClickT",s="onClickE",t="page",u="packageId",v="82240e52d7f84d438fb1d54011833b5d",w="type",x="Axure:Page",y="name",z="Page 1",A="notes",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="near",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageHorizontalAlignment",N="imageVerticalAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="adaptiveStyles",Z="interactionMap",ba="diagram",bb="objects",bc="masters",bd="objectPaths";
+return _creator();
+})());
